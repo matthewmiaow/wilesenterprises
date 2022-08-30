7599f77249c04ca535a0f32c2820469cb41bf674
Ben Wiles <benjay126@gmail.com>
2022-08-30 19:26:06 +1200

    mail improved

--- assets/script/contact.php
+++ assets/script/contact.php
@@ -1,12 +1,13 @@
 <?php
-   $to_email = "benwiles@wilesenterprises.com";
-   $subject = 'Contact to Wiles Enterprises - '.$_POST['firstname'].' '.$_POST['lastname'];
-   $body = $_POST['subject'];
-   $headers = "From: ".$_POST['emailaddress'];
+    $to_email = "benwiles@wilesenterprises.com";
+    $subject = 'Contact to Wiles Enterprises - '.$_POST['firstname'].' '.$_POST['lastname'];
+    $body = $_POST['subject'];
+    $headers = "From: ".$_POST['emailaddress'];
  
-   if ( mail($to_email, $subject, $body, $headers)) {
-      echo("Email successfully sent to $to_email...");
-   } else {
-      echo("Email sending failed...");
-   }
+    if ( mail($to_email, $subject, $body, $headers)) {
+        header("Location: https://example.com/myOtherPage.php");
+        die();
+    } else {
+        echo("Email sending failed...");
+    }
 ?>
