245fd35acebb6347cc33a1de6f5d5d6cd0b30002
Ben Wiles <benjay126@gmail.com>
2022-08-31 08:48:46 +1200

    php tset

--- assets/script/contact.php
+++ assets/script/contact.php
@@ -28,3 +28,17 @@
 
                 <input type="submit" value="Submit">
                 </form>
+
+
+                
+        if(isset($_POST['firstname'])) {
+            $to_email = "benwiles@wilesenterprises.com";
+            $subject = 'Contact to Wiles Enterprises - '.$_POST['firstname'].' '.$_POST['lastname'];
+            $body = $_POST['subject'];
+            $headers = "From: ".$_POST['emailaddress'];
+            if ( mail($to_email, $subject, $body, $headers)) {
+	            header("refresh:0;url=https://wilesenterprises.com/contact-confirm.html");
+            } else {
+                echo("Email sending failed...");
+            }
+        }
--- contact.php
+++ contact.php
@@ -6,17 +6,6 @@
     <title>Wiles Enterprises</title>
     <link rel="stylesheet" href="assets/style/header.css" />
     <?php
-        if(isset($_POST['firstname'])) {
-            $to_email = "benwiles@wilesenterprises.com";
-            $subject = 'Contact to Wiles Enterprises - '.$_POST['firstname'].' '.$_POST['lastname'];
-            $body = $_POST['subject'];
-            $headers = "From: ".$_POST['emailaddress'];
-            if ( mail($to_email, $subject, $body, $headers)) {
-	            header("refresh:0;url=https://wilesenterprises.com/contact-confirm.html");
-            } else {
-                echo("Email sending failed...");
-            }
-        }
     ?>
     <style>
         input[type=text], select, textarea {
