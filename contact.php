bec68a23f46ae4b84bde58616b9986b91151263b
Ben Wiles <benjay126@gmail.com>
2022-08-30 19:42:40 +1200

    php test

--- contact.php
+++ contact.php
@@ -72,16 +72,12 @@
 
     <?php
         if(isset($_POST['submit'])) {
+            echo ("isset");
             $to_email = "benwiles@wilesenterprises.com";
             $subject = 'Contact to Wiles Enterprises - '.$_POST['firstname'].' '.$_POST['lastname'];
             $body = $_POST['subject'];
             $headers = "From: ".$_POST['emailaddress'];
 
-            if ( mail($to_email, $subject, $body, $headers)) {
-                echo("Email sent successfully");
-            } else {
-                echo("Email sending failed...");
-            }
         }
     ?>
 
