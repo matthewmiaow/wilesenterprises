ac76b304f09a1ff008dd6b6ffbde0286deb4a9d1
Ben Wiles <benjay126@gmail.com>
2022-08-30 18:59:26 +1200

    php test

--- assets/script/contact.php
+++ assets/script/contact.php
@@ -1,4 +1,7 @@
 <?php
    $to_email = "benwiles@wilesenterprises.com";
-   echo $to_email;
+   $subject = 'Contact from Wiles Enterprises - '.$_POST['fname'].' '.$_POST['lname'];
+   $body = $_POST['subject'];
+   $headers = "From: ".$_POST['email'];
+   echo $subject;
 ?>
