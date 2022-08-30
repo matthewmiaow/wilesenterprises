2ac4defb4b3261560393a2b706860640c95edac8
Ben Wiles <benjay126@gmail.com>
2022-08-31 08:53:22 +1200

    php form added

--- assets/script/contact.php
+++ assets/script/contact.php
@@ -10,24 +10,7 @@
     }
 ?>
 
-<form method="post">
-                <label for="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="First Name">
 
-                <label for="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Last Name">
-
-                <label for="email">Email Address</label>
-                <input type="text" id="email" name="emailaddress" placeholder="Email Address">
-
-                <label for="pnum">Phone Number</label>
-                <input type="text" id="pnum" name="phonenumber" placeholder="Phone Number">
-
-                <label for="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-                <input type="submit" value="Submit">
-                </form>
 
 
                 
