f394bbd7fa09bc1ca56239b2655d5ea8076ad01b
Ben Wiles <benjay126@gmail.com>
2022-09-01 10:41:58 +1200

    php test

--- contact.php
+++ contact.php
@@ -100,7 +100,7 @@
         echo("Invalid phone number</br>");
         }
         echo("not empty posts</br>");
-        echo($_POST['validform']."</br>");
+        echo($_POST['validform']);
         }
         if(empty($_POST) or $_POST['validform'] == 'off') {
         echo('<form method="post">
