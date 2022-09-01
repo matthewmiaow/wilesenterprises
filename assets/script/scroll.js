eb3909eb44e24915b4881fbe67a6c3b8d119b96d
Ben Wiles <benjay126@gmail.com>
2022-09-01 14:32:27 +1200

    git test

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,16 +1,13 @@
-const checkpoint = 300;
+const checkpoint = 500;
 
-window.onscroll = function () {
-    scrollRotate();
-};
-
-function scrollRotate() {
+window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
     if (currentScroll <= checkpoint) {
         opacity = 1 - currentScroll / checkpoint;
     } else {
         opacity = 0;
     }
+    document.querySelector(".title").style.marginTop = ((opacity) * 30) + 'vh';
     document.querySelector(".title").style.opacity = opacity;
     document.querySelector(".subtitle").style.opacity = opacity;
-}
+});
