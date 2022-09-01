6e120adecc046673a3726f68aaae1ee78cec2322
Ben Wiles <benjay126@gmail.com>
2022-09-01 12:41:32 +1200

    scroll fade test

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,8 +1,16 @@
+const checkpoint = 300;
+
 window.onscroll = function () {
     scrollRotate();
 };
 
 function scrollRotate() {
-    let image = document.getElementById("spotlight");
-    image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= checkpoint) {
+        opacity = 1 - currentScroll / checkpoint;
+    } else {
+        opacity = 0;
+    }
+    document.querySelector(".title").style.opacity = opacity;
+    document.querySelector(".subtitle").style.opacity = opacity;
 }
