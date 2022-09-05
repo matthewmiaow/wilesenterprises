acef0124a2b3ff0d7c508ebe696f52bca9a68d2a
Ben Wiles <benjay126@gmail.com>
2022-09-06 11:48:43 +1200

    git push

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,23 +1,29 @@
 const checkpoint = 500;
+var movingClass = "scroll-move";
+var opacityClass = "scroll-opacity";
 
-const movingClass = "scroll-move";
-const opacityClass = "scroll-opacity";
+function addScrollFade(moveClass, opacityClass, checkpoint) {
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll <= checkpoint) {
+            opacity = 1 - currentScroll / checkpoint;
+        } else {
+            opacity = 0;
+        }
 
-window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll <= checkpoint) {
-        opacity = 1 - currentScroll / checkpoint;
-    } else {
-        opacity = 0;
-    }
+        var movingElems = document.getElementsByClassName(moveClass);
+        for (var i = 0; i < movingElems.length; i++) {
+            movingElems[i].style.marginTop.split('');
+            movingElems[i].style.marginTop = ((opacity) * 30) + 'vh';
+        }
 
-    var movingElems = document.getElementsByClassName(movingClass);
-    for (var i = 0; i < movingElems.length; i++) {
-        movingElems[i].style.marginTop = ((opacity) * 30) + 'vh';
-    }
+        var opacityElems = document.getElementsByClassName(opacityClass);
+        for (var i = 0; i < opacityElems.length; i++) {
+            opacityElems[i].style.opacity = opacity;
+        }
+    });
 
-    var opacityElems = document.getElementsByClassName(opacityClass);
-    for (var i = 0; i < opacityElems.length; i++) {
-        opacityElems[i].style.opacity = opacity;
-    }
-});
+}
+
+addScrollFade("scroll-move-1", "scroll-opacity-1", 200);
+addScrollFade("scroll-move-2", "", 500);
